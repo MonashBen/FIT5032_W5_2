dbc4ccbf2410e818501cc1e0b24972af04f9707d
Benson Lin <blin0008@student.monash.edu>
2021-08-31 18:57:18 +1000

    Add project files.

---
+++ FIT5032_Date_Picker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_Date_Picker.MvcApplication" Language="C#" %>
---
+++ FIT5032_Date_Picker/Models/Patient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_Date_Picker.Models
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+
+        public DateTime DOB { get; set; } 
+    }
+}
